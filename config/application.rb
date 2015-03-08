a8ccc289369a3259ce5a08523fa5b8462270115c
Kristin Gonzalez <kristin_gonzalez@yahoo.com>
2015-03-08 17:05:23 +0800

    Serve static assets

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,6 @@ module Mox
   config.autoload_paths += %W(\#{config.root}/lib)
   config.active_record.raise_in_transactional_callbacks = true
   config.middleware.use Rack::Attack
+  config.serve_static_assets = true
   end
 end
